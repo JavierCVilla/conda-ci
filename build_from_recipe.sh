699a4e07e9e37b3ca3ccc6342c28978c2ca6a4c8
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2019-04-12 13:43:22 +0200

    Add script to build root with conda

---
+++ build_from_recipe.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script setups the environment to use conda and 
+# conda-builds root using a custom recipe (with gtests)
+#
+# This script is meant to be run on a docker container
+#
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Enable conda
+. /opt/conda/etc/profile.d/conda.sh
+conda activate base
+
+conda install --yes --quiet conda-build make git
+git clone http://github.com/bluehood/root-feedstock
+conda build -c conda-forge root-feedstock/recipe
